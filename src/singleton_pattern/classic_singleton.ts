1246cc6d1eeaa9ed3b9963ee01a1cf112d60dd41
Ahmad-Dorri <ahmad.biamooz@gmail.com>
2026-07-08 06:55:36 +0330

    Add some note for using singleton pattern

--- src/singleton_pattern/classic_singleton.ts
+++ src/singleton_pattern/classic_singleton.ts
@@ -1,4 +1,5 @@
 // this is lazy instantiation. If we don't need this class we don't create it.
+// in multi threaded languages we may have race condition using singleton pattern. we should check for it and fix it. In languages like JS and dart that are single threaded langs, we don't have this issue. We may get error if we do some async work but we can remove that error by using module pattern singleton. worker and isolates are running on different threads but they have isolated memory and don't share memory with main thread.
 export class Singleton {
   static instance?: Singleton;
 
