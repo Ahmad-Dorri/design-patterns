b59da13c4d0df4da7e149091fcbce68274279432
Ahmad-Dorri <ahmad.biamooz@gmail.com>
2026-07-08 06:18:53 +0330

    Add classic singleton

---
+++ src/singleton_pattern/classic_singleton.ts
@@ -0,0 +1,19 @@
+export class Singleton {
+  static instance?: Singleton;
+
+  constructor() {
+    if (Singleton.instance) {
+      return Singleton.instance;
+    }
+
+    Singleton.instance = this;
+  }
+
+  static getInstance() {
+    if (!Singleton.instance) {
+      Singleton.instance = new Singleton();
+    }
+
+    return Singleton.instance;
+  }
+}
