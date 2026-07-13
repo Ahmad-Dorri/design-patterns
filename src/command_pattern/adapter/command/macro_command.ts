19d99779436ec1180e9e8def8dc509e418814603
Ahmad-Dorri <ahmad.biamooz@gmail.com>
2026-07-13 06:30:50 +0330

    Add macro command class

---
+++ src/command_pattern/adapter/command/macro_command.ts
@@ -0,0 +1,17 @@
+import type { Command } from "./command";
+
+export class MacroCommand implements Command {
+  private commands: Command[];
+
+  constructor(commands: Command[]) {
+    this.commands = commands;
+  }
+
+  execute(): void {
+    this.commands.forEach(command => command.execute());
+  }
+
+  undo(): void {
+    this.commands.forEach(command => command.undo());
+  }
+}
